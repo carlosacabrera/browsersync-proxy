d9b572dcec855402de98b2c5b9d91f2ae9e71ef4
Carlos A. Cabrera <ccabrera@sapient.com>
2016-02-12 19:36:22 -0500

    clean up

--- gulpfile.js/tasks/_main.js
+++ gulpfile.js/tasks/_main.js
@@ -2,13 +2,9 @@ var gulp = require('gulp');
 var config = require('../gulpconfig');
 var $ = require('gulp-load-plugins')(config.plugins);
 
-//console.log('---');
-//console.log($);
-//console.log('---');
 
 /*
  * Tasks
  */
-
-
+ 
 gulp.task('build', ['javascript', 'sass', 'html', 'fonts', 'images']);
