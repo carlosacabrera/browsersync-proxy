5ab29ee96af0146d035074171009802614a3829d
Giuliano Kranevitter <gkranevitter@sapient.com>
2016-02-17 14:33:02 -0500

    Testing added

---
+++ tests/spec/controllers/homeCtrl.test.js
@@ -0,0 +1,10 @@
+
+describe('Controller: Home', function () {
+
+    beforeEach(module('BostwickStudioApp'));
+
+    it('should be true', function () {
+        expect(true).toBe(true);
+    });
+
+});
